efe64d8b79e38b7f2851723da4b4ec1f926fc8b1
Richard Egeli <bioimtlaz@gmail.com>
2023-03-08 19:32:31 +0100

    test: contributions

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,8 @@ export * from './dom-component';
 export type StyleSheet = Partial<ElementCSSInlineStyle['style']>;
 export type CSS = StyleSheet | Record<string, StyleSheet>;
 
+export interface Menu {}
+
 export interface Directory {
   name: string;
   path: string;
